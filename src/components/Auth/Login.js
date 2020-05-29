62d31bd29cde68d1e0485c7e1816269758995bfe
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-05-29 12:14:13 +0530

    designed login page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login(props) {
-  return <div>Login</div>;
+  const [login, setLogin] = useState(true);
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form className="flex flex-column">
+        {!login && (
+          <input type="text" placeholder="Your Name" autoComplete="off" />
+        )}
+        <input type="email" placeholder="Your Email" autoComplete="off" />
+        <input type="password" placeholder="Choose a secure password" />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin((prevLogin) => !prevLogin)}
+          >
+            {login ? "need to create an account?" : "already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
