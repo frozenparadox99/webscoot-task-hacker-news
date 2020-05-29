fa5987dc073b89c7d9663a17d3224396cbc9f3e5
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-05-29 22:38:21 +0530

    init firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyABPhRn1GSrJCFCTPis0N4WZLkL1v4eres",
+  authDomain: "hooks-news-app-811d6.firebaseapp.com",
+  databaseURL: "https://hooks-news-app-811d6.firebaseio.com",
+  projectId: "hooks-news-app-811d6",
+  storageBucket: "hooks-news-app-811d6.appspot.com",
+  messagingSenderId: "275892740185",
+  appId: "1:275892740185:web:5c087659f161fc04ebd148",
+};
+
+export default firebaseConfig;
