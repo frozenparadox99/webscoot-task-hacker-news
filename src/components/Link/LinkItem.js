884d2d08cd8eefc1e5da5e74731583462fa09653
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-05-30 16:48:05 +0530

    added pagination

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -18,7 +18,8 @@ function LinkItem({ link, index, showCount, history }) {
           const previousVotes = doc.data().votes;
           const vote = { votedBy: { id: user.uid, name: user.displayName } };
           const updatedVotes = [...previousVotes, vote];
-          voteRef.update({ votes: updatedVotes });
+          const voteCount = updatedVotes.length;
+          voteRef.update({ votes: updatedVotes, voteCount });
         }
       });
     }
@@ -58,7 +59,7 @@ function LinkItem({ link, index, showCount, history }) {
           <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 lh-copy gray">
-          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {link.voteCount} votes by {link.postedBy.name}{" "}
           {formatDistanceToNow(link.created)} ago
           {" | "}
           <Link to={`/link/${link.id}`}>
